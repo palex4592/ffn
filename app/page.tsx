8ae29d3394b503afb08689d60132b9450001cca1
palex4592 <palex459230@gmail.com>
2024-07-06 15:36:50 -0400

    borders

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
           className="w-full h-auto object-cover"
         />
       </div>
-      <div className="border-4 border-black mt-24 flex flex-col items-center w-[90%] min-w-[1227px] h-auto min-h-[1038.21px] aspect-[1227/1038.21]">
-        <div className="border-4 border-black mb-24 w-full min-w-[1227px] h-auto min-h-[392px] aspect-[1227/392]">
-          <div className="flex items-center justify-center relative border-4 border-black mb-24 w-full min-w-[1227px] h-auto min-h-[392px] aspect-[1227/392]">
+      <div className="mt-24 flex flex-col items-center w-[90%] min-w-[1227px] h-auto min-h-[1038.21px] aspect-[1227/1038.21]">
+        <div className="mb-24 w-full min-w-[1227px] h-auto min-h-[392px] aspect-[1227/392]">
+          <div className="flex items-center justify-center relative mb-24 w-full min-w-[1227px] h-auto min-h-[392px] aspect-[1227/392]">
             <Image
               src="/text1.png"
               alt="texte"
@@ -32,10 +32,10 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="border-4 border-black flex flex-col items-center w-[97%] min-w-[1204.59px] h-auto min-h-[560.21px] aspect-[1204.59/560.21]">
-          <div className="border-4 border-black flex flex-col items-center w-[47%] min-w-[561px] h-auto min-h-[120px] aspect-[561/120]">
-            <div className="relative border-4 border-black flex flex-row items-center justify-center w-full min-w-[561px] h-auto min-h-[76px] aspect-[561/76]">
-              <div className="relative my-4 mr-8 border-4 border-black flex flex-row items-center justify-center w-full min-w-[536px] h-auto min-h-[48px] aspect-[536/48]">
+        <div className=" flex flex-col items-center w-[97%] min-w-[1204.59px] h-auto min-h-[560.21px] aspect-[1204.59/560.21]">
+          <div className=" flex flex-col items-center w-[47%] min-w-[561px] h-auto min-h-[120px] aspect-[561/120]">
+            <div className="relative  flex flex-row items-center justify-center w-full min-w-[561px] h-auto min-h-[76px] aspect-[561/76]">
+              <div className="relative my-4 mr-8  flex flex-row items-center justify-center w-full min-w-[536px] h-auto min-h-[48px] aspect-[536/48]">
                 <Image
                   src="/Choisissez un profil.png"
                   alt="Choisissez un profil"
@@ -60,7 +60,7 @@ export default function Home() {
               className="w-[31%] h-auto mt-8"
             />
           </div>
-          <div className="mt-24 border-4 border-black flex flex-row w-full min-w-[1204.59px] h-auto min-h-[560.21px] aspect-[1204.59/560.21]">
+          <div className="mt-24 flex flex-row w-full min-w-[1204.59px] h-auto min-h-[560.21px] aspect-[1204.59/560.21]">
             <div className="mx-8 w-[30%] min-w-[360.51px] h-auto min-h-[353.21px] aspect-[360.51/353.21]">
               <Link href="/nager-et-pratiquer">
                 <Image
